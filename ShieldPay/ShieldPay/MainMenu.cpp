dd659a6d5a49f880141be42f1e95010d6f9b5f14
IDDimov21 <IDDimov21@codingburgas.bg>
2023-12-21 23:28:52 +0200

    design for home page, blocks, assets

--- ShieldPay/ShieldPay/MainMenu.cpp
+++ ShieldPay/ShieldPay/MainMenu.cpp
@@ -6,10 +6,16 @@ using namespace std;
 
 int main()
 {
-    const int screenWidth = 1200;
-    const int screenHeight = 800;
+    const int screenWidth = 1100;
+    const int screenHeight = 700;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    Texture2D NavBar = LoadTexture("Images/Bar.png");
+    Texture2D background = LoadTexture("Images/background.png");
+    Texture2D blockBG = LoadTexture("Images/blockBG.png");
+    Texture2D login = LoadTexture("Images/login.png");
+    Texture2D regis = LoadTexture("Images/register.png");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 
@@ -17,7 +23,11 @@ int main()
     {
 
         BeginDrawing();
-
+        DrawTexture(NavBar, 0, 0, RAYWHITE);
+        DrawTexture(background, 0, 87, RAYWHITE);
+        DrawTexture(blockBG, screenWidth / 2, 200, RAYWHITE);
+        DrawTexture(login, screenWidth / 2, 150, RAYWHITE);
+        DrawTexture(regis, screenWidth / 2, 250, RAYWHITE);
         ClearBackground(RAYWHITE);
 
 
