77b8ffc6ce612440849c197a0115af4cdabbdff3
IDDimov21 <IDDimov21@codingburgas.bg>
2023-12-22 16:02:15 +0200

    add & finish text input rec + logic

--- ShieldPay/ShieldPay/MainMenu.cpp
+++ ShieldPay/ShieldPay/MainMenu.cpp
@@ -17,19 +17,70 @@ int main()
     Texture2D login = LoadTexture("Images/login.png");
     Texture2D regis = LoadTexture("Images/register.png");
 
+    char text[21] = { 0 };  // Array to store input text (plus one for the null terminator)
+    int textSize = 0;        // Variable to keep track of text size
+    bool isTextBoxFocused = false;
+    int framesCounter = 0;
+    bool cursorVisible = true;
+
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 
     while (!WindowShouldClose())   
     {
 
         BeginDrawing();
+        ClearBackground(RAYWHITE);
         DrawTexture(NavBar, 0, 0, RAYWHITE);
         DrawTexture(background, 0, 87, RAYWHITE);
         DrawTexture(blockBG, screenWidth / 2, 200, RAYWHITE);
         DrawTexture(login, screenWidth / 2 + 210, 365, RAYWHITE);
         DrawTexture(regis, screenWidth / 2 + 300, 420, RAYWHITE);
-        ClearBackground(RAYWHITE);
 
+        Rectangle textBox = { 10, 10, 300, 40 };
+        DrawRectangleRec(textBox, isTextBoxFocused ? SKYBLUE : LIGHTGRAY);
+        DrawRectangleLinesEx(textBox, 2, isTextBoxFocused ? DARKBLUE : DARKGRAY);
+
+
+        DrawText(text, 20, 20, 20, BLACK);
+
+        if (isTextBoxFocused && cursorVisible) {
+            int cursorX = MeasureText(text, 20) + 20;
+            DrawLine(cursorX, 20, cursorX, 40, BLACK);
+        }
+
+        if (CheckCollisionPointRec(GetMousePosition(), textBox)) {
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                isTextBoxFocused = !isTextBoxFocused;
+            }
+        }
+        else if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            isTextBoxFocused = false;
+        }
+
+        if (isTextBoxFocused) {
+            framesCounter++;
+
+            if (framesCounter >= 30) {
+                framesCounter = 0;
+                cursorVisible = !cursorVisible;
+            }
+
+            int key = GetKeyPressed();
+            if (key != 0) {
+                if ((key >= 32) && (key < 127) && (textSize < 20)) {
+                    text[textSize] = (char)key;
+                    textSize++;
+                }
+                else if (key == KEY_BACKSPACE && textSize > 0) {
+                    textSize--;
+                    text[textSize] = '\0';
+                }
+            }
+        }
+        else {
+            framesCounter = 0;
+            cursorVisible = true;
+        }
 
         EndDrawing();
     }
