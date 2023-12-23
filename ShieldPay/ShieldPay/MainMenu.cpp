be82ba2a3684d0be14a4be73530c6da68c8ad2dd
IDDimov21 <IDDimov21@codingburgas.bg>
2023-12-23 23:12:42 +0200

    poistion input box

--- ShieldPay/ShieldPay/MainMenu.cpp
+++ ShieldPay/ShieldPay/MainMenu.cpp
@@ -1,9 +1,46 @@
-
-#include <iostream>
-#include "raylib.h"
-
+#include "mainMenu.hpp"
 using namespace std;
 
+//void Input() {
+//    if (isTextBoxFocused && cursorVisible) {
+//        int cursorX = MeasureText(text, 20) + 20;
+//        DrawLine(cursorX, 20, cursorX, 40, BLACK);
+//    }
+//    if (CheckCollisionPointRec(GetMousePosition(), textBox)) {
+//        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+//            isTextBoxFocused = !isTextBoxFocused;
+//        }
+//    }
+//    else if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+//        isTextBoxFocused = false;
+//    }
+//
+//    if (isTextBoxFocused) {
+//        framesCounter++;
+//        if (framesCounter >= 30) {
+//            framesCounter = 0;
+//            cursorVisible = !cursorVisible;
+//        }
+//        int key = GetKeyPressed();
+//        if (key != 0) {
+//            if ((key >= 32) && (key < 127) && (textSize < 16)) {
+//                text[textSize] = (char)key;
+//                textSize++;
+//            }
+//            else if (key == KEY_BACKSPACE && textSize > 0) {
+//                textSize--;
+//                text[textSize] = '\0';
+//            }
+//        }
+//    }
+//
+//    else {
+//        framesCounter = 0;
+//        cursorVisible = true;
+//    }
+//}
+
+/*IM ON THAT GOOD KUSH AND ALCOHOL I GOT SOME DOWN BITCHES I CAN CALL IDK WHAT I WOULD W / O YALL*/
 int main()
 {
     const int screenWidth = 1100;
@@ -36,51 +73,48 @@ int main()
         DrawTexture(login, screenWidth / 2 + 210, 365, RAYWHITE);
         DrawTexture(regis, screenWidth / 2 + 300, 420, RAYWHITE);
 
-        Rectangle textBox = { 10, 10, 300, 40 };
+        Rectangle textBox = { screenWidth / 2 + 180, screenHeight / 2 - 95 , 240, 40 };
         DrawRectangleRec(textBox, isTextBoxFocused ? SKYBLUE : LIGHTGRAY);
         DrawRectangleLinesEx(textBox, 2, isTextBoxFocused ? DARKBLUE : DARKGRAY);
 
+        DrawText(text, screenWidth / 2 + 187, screenHeight / 2 - 85, 20, BLACK);
 
-        DrawText(text, 20, 20, 20, BLACK);
-
-        if (isTextBoxFocused && cursorVisible) {
-            int cursorX = MeasureText(text, 20) + 20;
-            DrawLine(cursorX, 20, cursorX, 40, BLACK);
-        }
-
-        if (CheckCollisionPointRec(GetMousePosition(), textBox)) {
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                isTextBoxFocused = !isTextBoxFocused;
+            if (isTextBoxFocused && cursorVisible) {
+                int cursorX = MeasureText(text, 20) + 20;
+                DrawLine(cursorX, 20, cursorX, 40, BLACK);
             }
-        }
-        else if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            isTextBoxFocused = false;
-        }
-
-        if (isTextBoxFocused) {
-            framesCounter++;
-
-            if (framesCounter >= 30) {
+            if (CheckCollisionPointRec(GetMousePosition(), textBox)) {
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                    isTextBoxFocused = !isTextBoxFocused;
+                }
+            }
+            else if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                isTextBoxFocused = false;
+            }
+        
+            if (isTextBoxFocused) {
+                framesCounter++;
+                if (framesCounter >= 30) {
+                    framesCounter = 0;
+                    cursorVisible = !cursorVisible;
+                }
+                int key = GetKeyPressed();
+                if (key != 0) {
+                    if ((key >= 32) && (key < 127) && (textSize < 16)) {
+                        text[textSize] = (char)key;
+                        textSize++;
+                    }
+                    else if (key == KEY_BACKSPACE && textSize > 0) {
+                        textSize--;
+                        text[textSize] = '\0';
+                    }
+                }
+            }
+        
+            else {
                 framesCounter = 0;
-                cursorVisible = !cursorVisible;
+                cursorVisible = true;
             }
-
-            int key = GetKeyPressed();
-            if (key != 0) {
-                if ((key >= 32) && (key < 127) && (textSize < 20)) {
-                    text[textSize] = (char)key;
-                    textSize++;
-                }
-                else if (key == KEY_BACKSPACE && textSize > 0) {
-                    textSize--;
-                    text[textSize] = '\0';
-                }
-            }
-        }
-        else {
-            framesCounter = 0;
-            cursorVisible = true;
-        }
 
         EndDrawing();
     }
---
+++ ShieldPay/ShieldPay/mainMenu.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include "raylib.h"
+#include <string>
+
+int main();
+void Input();
