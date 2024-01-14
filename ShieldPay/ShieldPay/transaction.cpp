364566b4d0f340ff78a32bea66a51f0abb8f8ab8
KRRusev21 <107102860+KRRusev21@users.noreply.github.com>
2024-01-14 19:46:25 +0200

    Transaction logic orgranisation

--- ShieldPay/ShieldPay/transaction.cpp
+++ ShieldPay/ShieldPay/transaction.cpp
@@ -1,50 +1,59 @@
 #include "transaction.hpp"
 
-void transaction(const std::string& username, const std::string & password, double& balance, bool& isSendPressed, string& pass, string& user, string& sum, string& dataFolderPath) {
-        
-        string usernameFromFile, balanceFromFile;   
-        fstream Usernames, Balances;
-        double tempBal;
-        if (isSendPressed) {
-            if (pass == password) {
-                if (user == username) {
-                    cout << "You can't send money to yourself." << endl;
-                }
-                else {
-                    if (balance > stod(sum)) {
-                        Usernames.open(dataFolderPath + "/usernames.txt", ios::in);
-                        Balances.open(dataFolderPath + "/balances.txt", ios::in | ios::out);
-                        balance -= stod(sum);
-                        while (getline(Usernames, usernameFromFile) && getline(Balances, balanceFromFile))
-                        {
-                            tempBal = stod(balanceFromFile);
-                            //if (usernameFromFile == username) {
-                            //    Balances << balance;
-                            //}
-                            if (usernameFromFile == user) {
-                                cout << "Entered Username: " << username << endl;
-                                cout << "Entered Balance: " << balance << endl;
-                                tempBal += stod(sum);
-                                balanceFromFile = to_string(tempBal);
-                                Balances << balanceFromFile;
-                                Usernames.close();
-                                Balances.close();
-                                isSendPressed = false;
-                            }
-                        }
-                        Usernames.close();
-                        Balances.close();
-                        isSendPressed = false;
-                    }
-                    else {
-                        cout << "Not enough money to make this transaction!" << endl;
-                        isSendPressed = false;
-                    }
-                }
+void transaction(const std::string& username, const std::string& password, double& balance, bool& isSendPressed, string& pass, string& user, string& sum, string& dataFolderPath) {
+
+    string usernameFromFile, balanceFromFile;
+    fstream Usernames, Balances;
+    bool check1 = false, check2 = false;
+    double tempBal;
+    if (isSendPressed) {
+        if (pass == password) {
+            if (user == username) {
+                cout << "You can't send money to yourself." << endl;
             }
             else {
-                cout << "Wrong password!!" << endl;
-                isSendPressed = false;
+                if (balance > stod(sum)) {
+                    Usernames.open(dataFolderPath + "/usernames.txt", ios::in);
+                    Balances.open(dataFolderPath + "/balances.txt", ios::app);
+                    balance -= stod(sum);
+                    while (getline(Usernames, usernameFromFile) && getline(Balances, balanceFromFile))
+                    {
+                        tempBal = stod(balanceFromFile);
+
+                        if (usernameFromFile == username && !check1) {
+                            cout << balance;
+                            Balances << balance;
+                        }
+                        else if (usernameFromFile == user && !check2) {
+                            tempBal += stod(sum);
+                            balanceFromFile = to_string(tempBal);
+                            cout << balanceFromFile;
+                            Balances << balanceFromFile;
+                        }
+                        else if (usernameFromFile == "") {
+                            cout << "Non existing username" << endl;
+                            isSendPressed = false;
+                        }
+                        else if (check1 && check2) {
+                            Usernames.close();
+                            Balances.close();
+                            isSendPressed = false;
+                            break;
+                        }
+                    }
+                    Usernames.close();
+                    Balances.close();
+                    isSendPressed = false;
                 }
+                else {
+                    cout << "Not enough money to make this transaction!" << endl;
+                    isSendPressed = false;
+                }
+            }
         }
- }
+        else {
+            cout << "Wrong password!!" << endl;
+            isSendPressed = false;
+        }
+    }
+}
